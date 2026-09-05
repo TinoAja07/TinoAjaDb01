a92199436e51322715f63b290e42c33b7bf34dfd
Tino Aja <142353452+TinoAja07@users.noreply.github.com>
2026-09-05 07:50:52 +0700

    Add configuration file for access control list

---
+++ configClient.js
@@ -0,0 +1,14 @@
+// CONFIGURATION FILE: MANAGED BY CHRISTINO MALAYSIANTO
+// ====================================================
+
+const ACCESS_CONTROL_LIST = {
+    // Format: "nama_folder_di_github": "domain_patreon_klien"
+    "pesanan01": "://patreon.com",
+    "pesanan02": "://patreon.com",
+    
+    // Jangan hapus baris di bawah ini agar Anda bisa tetap tes offline di Linux
+    "test": "localhost" 
+};
+
+// Ekspor agar bisa dibaca oleh index.html
+window.ACCESS_CONTROL_LIST = ACCESS_CONTROL_LIST;
